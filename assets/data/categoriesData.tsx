4c71d9ac7f5f8d4d83c1866a69eb27716c89e118
JAY KIM <jaykim@JAYs-MacBook-Pro-2018.local>
2022-05-06 06:35:39 -0400

    Adding sample data

---
+++ assets/data/categoriesData.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable global-require */
+export const categoriesData = [
+  {
+    id: '1',
+    image: require('../images/pizza-icon.png'),
+    title: 'Pizza',
+    selected: true,
+  },
+  {
+    id: '2',
+    image: require('../images/shrimp-icon.png'),
+    title: 'Seafood',
+    selected: false,
+  },
+  {
+    id: '3',
+    image: require('../images/soda-icon.png'),
+    title: 'Soft Drinks',
+    selected: false,
+  },
+];
